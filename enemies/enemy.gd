6ae2fc847e8eff52e7101b065f04e634a28a84c2
Harvey Thomas <harvey.dpt@outlook.com>
2024-07-11 20:17:10 +0100

    added basic boid behavior, refactor needed

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,10 +6,11 @@ extends CharacterBody2D
 @export var max_health: float
 @export var attack_damage: float
 
-var cohesion: float = 1
-var alignment: float = 0.2
-var separation: float = 30
-var follow: float = 2
+var centering_factor: float = 0.05
+var min_distance: float = 250
+var avoid_factor: float = 0.05
+var matching_factor: float = 0.05
+var chasing_factor: float = 0.3
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: Player
@@ -26,34 +27,52 @@ func _ready():
 
 
 func update(positions: Array, velocities: Array) -> void:
-	var cohesion_vec: Vector2 = positions.reduce(func(acc, vec): return acc + vec.normalized()) / positions.size() - position
-	var alignment_vec: Vector2 = velocities.reduce(func(acc, vec): return acc + vec.normalized()) / velocities.size() - position
-	
-	cohesion_vec = cohesion_vec.normalized()
-	alignment_vec = alignment_vec.normalized()
-	
-	var separation_vec: Vector2 = Vector2.ZERO
+	move_towards_center(positions)
+	avoid_others(positions)
+	match_velocity(velocities)
+	chase_player()
 
-	for pos in positions:
-		if pos == position:
-			continue
-		
-		var opposite_vec: Vector2 = position - pos
-
-		if opposite_vec.length() < separation:
-			separation_vec += opposite_vec.normalized() * (separation - opposite_vec.length())
-	
-	var target_vec = (player.position - position).normalized()
-
-	velocity = ((cohesion_vec * cohesion + alignment_vec * alignment) + target_vec * follow) / 2 + separation_vec
-	
 	velocity = velocity.normalized() * speed
-	
-	print(velocity.normalized() - (player.position - position).normalized())
 	
 	move_and_slide()
 	
 	look_at(player.position)
+
+
+func move_towards_center(positions: Array):
+	var center: Vector2 = Vector2.ZERO
+
+	for pos in positions:
+		center += pos
+	
+	center /= positions.size()
+
+	velocity += center * centering_factor
+
+
+func avoid_others(positions: Array):
+	var move: Vector2 = Vector2.ZERO
+
+	for pos in positions:
+		if pos != position and (position - pos).length() < min_distance:
+			move += position - pos
+	
+	velocity += move * avoid_factor
+
+
+func match_velocity(velocities: Array):
+	var average: Vector2 = Vector2.ZERO
+
+	for vel in velocities:
+		average += vel
+	
+	average /= velocities.size()
+
+	velocity += (velocity - average) * matching_factor
+
+
+func chase_player():
+	velocity += (player.position - position) * chasing_factor
 
 
 func handle_bullet_hit(damage: float) -> void:
